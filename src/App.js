09a8530dec7173cc2aecc6e71c05cdcc97427c5b
Bryan <bryan.liao@stonybrook.edu>
2018-02-03 14:34:39 -0500

    facebook api adjusting

--- src/App.js
+++ src/App.js
@@ -12,25 +12,23 @@ class App extends Component {
 
 
   fetchData(){
-    fetch('https://api-wufthacks.xlabs.one:8243/facebookGraphAPI/1.0.0/v2.11/me?access_token=EAACD7kf3Pg8BANrTtBmJRfyk2gHYjSY1kWx9lCoHNPPak3osdyYHIh8kV8IfQrUSpvRYMRuZCQSqpYa4fPeImHYlH3TFu4EOue2Re56EZCpIAQjWlufIM4HuZCAItt8O5jxEZB2ZAv4T933jCIeb3ZBqjOFNG5dBmrOTU1o8hyiwZDZD&fields=cover%2Cname%2Cid%2Cabout', {
+    fetch('https://api-wufthacks.xlabs.one:8243/facebookGraphAPI/1.0.0/v2.11/me?access_token=EAACD7kf3Pg8BANrTtBmJRfyk2gHYjSY1kWx9lCoHNPPak3osdyYHIh8kV8IfQrUSpvRYMRuZCQSqpYa4fPeImHYlH3TFu4EOue2Re56EZCpIAQjWlufIM4HuZCAItt8O5jxEZB2ZAv4T933jCIeb3ZBqjOFNG5dBmrOTU1o8hyiwZDZD&fields=cover%2Cname%2Cid%2Cabout%2C%20gender', {
       headers: {
         "Accept": "application/json",
-        "X-Api-Key": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYW1lc2FydGh1ciIsImV4cCI6MTUxODUzMjIxOH0.-RMBLl5LQXVSyK3HOaJtoz3tJzictVIYo_ePC-fcYGNDyFjQ8QK-zSnEiDbpWCvPafjG8g3ptz3fauvbcsvTGg",
+       // "X-Api-Key": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYW1lc2FydGh1ciIsImV4cCI6MTUxODUzMjIxOH0.-RMBLl5LQXVSyK3HOaJtoz3tJzictVIYo_ePC-fcYGNDyFjQ8QK-zSnEiDbpWCvPafjG8g3ptz3fauvbcsvTGg",
         "Authorization": "Bearer d515abaf-dcf7-341e-ac14-099b63c5c1c3",
        // "Cache-Control": "no-cache",
        // "Postman-Token": "f0136045-e306-276a-e410-efbea980d06a",
-        "Access-Control-Allow-Origin":'*'
+       // "Access-Control-Allow-Origin":'*'
       }
     })
     .then(response=>response.json())
-    .then(parsedJSON => parsedJSON.results.map(user =>({
-      name:`${user.gender}`
-        }
-      )))
-      .then(contacts => this.setState({contacts}))
-      .catch(error => console.log('parsing failed',error))
+    .then((responseData, err) => {
+      console.log(responseData.gender)
+    })
+      // .then(contacts => this.setState({contacts}))
+      // .catch(error => console.log('parsing failed',error))
   }
-
 
   render() {
     const {contacts} = this.state;
