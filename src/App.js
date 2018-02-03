1809b3fda011294c62244aa91ffdfa4078ec2724
Bryan <bryan.liao@stonybrook.edu>
2018-02-03 12:20:52 -0500

    something

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 import logo from './td.png';
 import './App.css';
+import Fetch from 'react-fetch';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {contacts: []}
+  }
+
+
+  fetchData(){
+    fetch('https://api-wufthacks.xlabs.one:8243/facebookGraphAPI/1.0.0/v2.11/me?access_token=EAACD7kf3Pg8BANrTtBmJRfyk2gHYjSY1kWx9lCoHNPPak3osdyYHIh8kV8IfQrUSpvRYMRuZCQSqpYa4fPeImHYlH3TFu4EOue2Re56EZCpIAQjWlufIM4HuZCAItt8O5jxEZB2ZAv4T933jCIeb3ZBqjOFNG5dBmrOTU1o8hyiwZDZD&fields=cover%2Cname%2Cid%2Cabout', {
+      headers: {
+        "Accept": "application/json",
+        "X-Api-Key": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYW1lc2FydGh1ciIsImV4cCI6MTUxODUzMjIxOH0.-RMBLl5LQXVSyK3HOaJtoz3tJzictVIYo_ePC-fcYGNDyFjQ8QK-zSnEiDbpWCvPafjG8g3ptz3fauvbcsvTGg",
+        "Authorization": "Bearer d515abaf-dcf7-341e-ac14-099b63c5c1c3",
+       // "Cache-Control": "no-cache",
+       // "Postman-Token": "f0136045-e306-276a-e410-efbea980d06a",
+        "Access-Control-Allow-Origin":'*'
+      }
+    })
+    .then(response=>response.json())
+    .then(parsedJSON => parsedJSON.results.map(user =>({
+      name:`${user.gender}`
+        }
+      )))
+      .then(contacts => this.setState({contacts}))
+      .catch(error => console.log('parsing failed',error))
+  }
+
+
   render() {
+    const {contacts} = this.state;
     return (
       /*
           <header className="App-header">
@@ -12,10 +42,22 @@ class App extends Component {
           </header>
         */
       <div className="App">
+              <button className="btn btn-sm btn-danger" onClick={(e) => {
+                        this.fetchData();    
+                    }}>Fetch now</button>
         <div className="App-container">
         <div className="App-sidebar"> <img src={logo} className="App-logo" alt="logo" /></div>
         <div className="App-title">Customer Profile</div>
         </div>
+        <div className="panel-group">
+       {
+           contacts.length > 0 ? contacts.map(contact => {
+               const {name} = contact;
+               return <h1 title={name}>
+               </h1>
+           }) : null
+       }
+   </div>
       </div>
     );
   }
@@ -24,19 +66,7 @@ class App extends Component {
 
 /*
 
-var data = null;
-
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4) {
-    console.log(this.responseText);
-  }
-});
-
-
-xhr.open("GET", "https://api-wufthacks.xlabs.one:8243/facebookGraphAPI/1.0.0/v2.11/me?access_token=EAACD7kf3Pg8BANrTtBmJRfyk2gHYjSY1kWx9lCoHNPPak3osdyYHIh8kV8IfQrUSpvRYMRuZCQSqpYa4fPeImHYlH3TFu4EOue2Re56EZCpIAQjWlufIM4HuZCAItt8O5jxEZB2ZAv4T933jCIeb3ZBqjOFNG5dBmrOTU1o8hyiwZDZD&fields=cover%2Cname%2Cid%2Cabout");
+xhr.open("GET", "");
 xhr.setRequestHeader("Accept", "text/javascript");
 xhr.setRequestHeader("Authorization", "Bearer d515abaf-dcf7-341e-ac14-099b63c5c1c3");
 xhr.setRequestHeader("Cache-Control", "no-cache");
